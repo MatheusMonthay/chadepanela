8ff969d2bf262c446ec4dbd1f57a4c333ba1f40f
Matheus Monthay <matheusqmmaw@gmail.com>
2024-09-26 19:44:13 -0400

    finalizando

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-document.getElementById("confirmButton").addEventListener("click", function() {
-    // Pergunta o nome do convidado
-    var name = prompt("Digite seu nome para confirmar a presença:");
+// Função para abrir o modal
+document.getElementById('modalButton').onclick = function() {
+    document.getElementById('confirmModal').style.display = 'block';
+}
 
-    if (name) {
-        // Mensagem personalizada para o WhatsApp
-        var message = "Olá, meu nome é " + name + " e estou confirmando minha presença no Chá de Panela de Matheus e Kariny!";
-        
-        // Link do WhatsApp com a mensagem predefinida
-        var whatsappLink = "https://api.whatsapp.com/send?phone=5588999999999&text=" + encodeURIComponent(message);
-
-        // Redireciona para o WhatsApp
-        document.getElementById("confirmButton").href = whatsappLink;
-    } else {
-        alert("Por favor, insira seu nome para confirmar.");
+// Para fechar o modal ao clicar fora dele
+window.onclick = function(event) {
+    if (event.target == document.getElementById('confirmModal')) {
+        document.getElementById('confirmModal').style.display = 'none';
     }
-});
+}
+
+// Função para confirmar presença
+function confirmarPresenca(phoneNumber) {
+    var message = "Olá, eu e minha família estamos muito felizes em fazer parte deste momento da vida de vocês, podem contar com nossa presença.";
+    var whatsappLink = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message);
+    window.location.href = whatsappLink;
+}
+
+// Adicionando ações aos botões do modal
+document.getElementById('confirmMatheus').onclick = function() {
+    confirmarPresenca("5569992834132"); // Chama a função com o número de Matheus
+    document.getElementById('confirmModal').style.display = 'none'; // Fecha o modal após a confirmação
+}
+
+document.getElementById('confirmKariny').onclick = function() {
+    confirmarPresenca("556984736855"); // Chama a função com o número de Kariny
+    document.getElementById('confirmModal').style.display = 'none'; // Fecha o modal após a confirmação
+}
